f78e318892f5afcb9651bed5dcaa8f40e8a634c3
saidlionsgeek <saidlionesgeek.1997@gmail.com>
2023-06-23 15:49:58 +0100

    javascript modal

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,59 @@
+// darck mode
+let btnMode = document.querySelector(".said-darck");
+
+btnMode.addEventListener("click",()=>{
+    document.querySelector("body").classList.toggle("sombre")
+})
+
+// modal conexion
+let btn_open_modal = document.querySelectorAll(".btn-open-modal");
+let div_modal = document.querySelectorAll(".div-modal");
+let btn_close_modal = document.querySelectorAll(".btn-close-modal");
+
+let myBody = document.body ;
+
+const openModal = (div) => {
+    div.classList.remove("modal-no-show");
+    div.classList.add("modal-show");
+    myBody.classList.add("body-modal-open");
+}
+
+const closeModal = (div) => {
+    div.classList.add("modal-no-show");
+    div.classList.remove("modal-show");
+    myBody.classList.remove("body-modal-open");
+}
+
+for (let index = 0; index < btn_open_modal.length; index++) {
+
+    let btnElementOpen = btn_open_modal[index];
+    let idBtnOpen = btnElementOpen.getAttribute("id");
+
+    for (let idx = 0; idx < div_modal.length; idx++) {
+        let divElement = div_modal[idx];
+        let idDiv = divElement.getAttribute("id");
+
+        if (idBtnOpen == idDiv) {
+            btn_open_modal[index].addEventListener("click" , () => {
+                openModal(div_modal[idx])
+            })
+        }
+
+        for (let i = 0; i < btn_close_modal.length; i++) {
+            let btnElementClose = btn_close_modal[i];
+            let idBtnClose = btnElementClose.getAttribute("id");
+
+            if (idBtnClose == idDiv) {
+                btn_close_modal[i].addEventListener("click" , () => {
+                    closeModal(div_modal[idx]);
+                })
+            }
+        }
+    }
+}
+
 // section 5 tabs
+
 let li_section5 = document.querySelectorAll(".li-section5");
 let content_tabs = document.querySelectorAll(".content");
 
